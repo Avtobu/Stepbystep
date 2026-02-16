b21eb2e12557d8c2214381f59066c4d88c8344d2
Avtobu <lifeivanna2007@gmail.com>
2026-02-16 13:26:15 +0200

    Update

--- web/models.py
+++ web/models.py
@@ -5,6 +5,6 @@ class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
+    user = db.Column(db.String(50), unique=True)
     email = db.Column(db.String(150), unique=True)
-    password_hash = db.Column(db.String(255))
+    password = db.Column(db.String(255))
