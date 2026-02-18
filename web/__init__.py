a624bc3d82e503673120d298cefbd0020b48b369
Avtobu <lifeivanna2007@gmail.com>
2026-02-18 23:58:16 +0200

    Added password checking, flask-mail

--- web/__init__.py
+++ web/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
+    mail = Mail(app)
+    mail.init_app(app)
     app.config['SECRET_KEY'] = 'secret'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USERNAME'] = os.getenv('DEL_EMAIL')
+    app.config['MAIL_PASSWORD'] = os.getenv('PASSWORD')
 
     db.init_app(app)
     login_manager.init_app(app)
--- web/auth.py
+++ web/auth.py
@@ -1,10 +1,47 @@
+import mail
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from .models import User
+from web.models import User
 from . import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Mail, Message
+import os
+import dotenv
 from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('user')
+        password = request.form.get('password')
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("Email is taken", category="error")
+            return render_template("signup.html")
+
+        strong, message = User().check_strongpassword(password)
+        if not strong:
+            flash(message, category="error")
+            return render_template("signup.html")
+
+        new_user = User(email=email, user=username)
+        new_user.set_password(password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(new_user)
+        return redirect(url_for('views.home'))
+    return render_template("signup.html")
+
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
@@ -12,28 +49,27 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
-        if user and check_password_hash(user.password, password):
+        if user and user.check_password(password):
             login_user(user)
             return redirect(url_for('views.home'))
-        flash("Wrong credentials")
+
+        flash("Wrong credentials", category="error")
+
     return "Login page"
 
-@auth.route('/signup', methods=['GET','POST'])
-def signup():
-    if request.method == 'POST':
-        user = request.form.get('user')
-        email = request.form.get('email')
-        password = request.form.get('password')
 
-        new_user = User(email=email, password=generate_password_hash(password), user = user)
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user)
-        return redirect(url_for('views.home'))
-    return render_template("signup.html")
+@auth.route("/")
 
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
+def index():
+    return render_template("index.html")
+
+@auth.route("/submit", methods=["POST"])   #is will be changed when i will get front
+def submit():
+    if request.method== "POST":
+        name= request.form["name"]
+        #subject request.form["subject"]
+        message=  request.form["message"]
+        msg = Message("Hello", sender=  os.getenv("EMAIL"), recipients  = [os.getenv('REC_EMAIL')])
+        msg.body = "Hello "+name+",\n\n"+message
+        mail.send(msg)
+    return redirect(url_for("index"))
