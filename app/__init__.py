a40a630a5132580ca894d38f43f7b18ceb278c92
Avtobu <lifeivanna2007@gmail.com>
2026-03-30 15:47:37 +0300

    edit init

--- app/__init__.py
+++ app/__init__.py
@@ -13,5 +13,12 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     csrf.init_app(app)
 
+    from app.auth import auth_bp
+    from app.main import main_bp
+    from app.api import api_bp
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(main_bp)
+    app.register_blueprint(api_bp, url_prefix="/api")
 
     return app
