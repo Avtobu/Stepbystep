5d69b530c79ce16f4bb47fa5526e0f850c1e5bb7
Avtobu <lifeivanna2007@gmail.com>
2026-05-01 19:18:43 +0300

    delete temples

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@ import os
 from flask import send_from_directory
 from app.main import main_bp
 
-DIST_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "static", "dist")
+DIST_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "client", "dist")
 
 @main_bp.route("/", defaults={"path": ""})
 @main_bp.route("/<path:path>")
